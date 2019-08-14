4353dc1f8572329a6bf3578b8999b4cc32c9f765
evansgithubaccount <48694606+evansgithubaccount@users.noreply.github.com>
2019-08-14 09:48:55 -0400

    Initial commit

---
+++ winfo/src/main/java/com/capstone/winfo/WinfoApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.winfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinfoApplication.class, args);
+    }
+
+}
---
+++ winfo/src/main/java/com/capstone/winfo/domain/User.java
@@ -0,0 +1,25 @@
+package com.capstone.winfo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private Boolean admin;
+}
