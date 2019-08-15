07e155ca29a15ef80281359146b7d25274f655f1
evansgithubaccount <48694606+evansgithubaccount@users.noreply.github.com>
2019-08-15 11:19:30 -0400

    added some security classes and Post methods

--- winfo/src/main/java/com/capstone/winfo/domain/posting/Post.java
+++ winfo/src/main/java/com/capstone/winfo/domain/posting/Post.java
@@ -7,7 +7,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -20,11 +23,23 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     private String title;
 
     private String description;
 
+    private String language;
+
+    private String topic;
+
+    private String subTopic;
+
+    @NotNull
     private String url;
+
+    @ElementCollection
+    @Builder.Default
+    private List<String> keywords = new ArrayList<>();
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
