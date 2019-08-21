43a3be3d4893c75383cac25bdc36107a70da2aaf
Landon Burnier <landonburnier@gmail.com>
2019-08-21 10:20:26 -0400

    page layouts

--- winfrontend/src/components/CSSPage.js
+++ winfrontend/src/components/CSSPage.js
@@ -12,24 +12,72 @@ class CSSPage extends React.Component {
 
     render() {
         return (
-            <div id='cssPage'>
-                <Navigation />
-                <div class="row">
-                    <div class="col-sm-2">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <Link class="nav-link active" to={{ pathname: '/javascript/react' }}>TBD</Link>
+            <div id='javaPage'>
+                <Navigation/>
+                <div className="row">
+                    {/*<div class="col-sm-2" id="sideBar">*/}
+                    <div className="col-sm-1">
+                        <ul className="nav flex-column">
+                            <li className="nav-item">
+                                <Link class="nav-link active" to={{pathname: '/javascript/react'}}>React</Link>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Other</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Other</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Other</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Other</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href="#">Other</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Other</a>
                             </li>
                         </ul>
+                    </div>
+                    {/*</div>*/}
+                    <div className="col-sm-9 stuff">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam facilisis tortor eu ex faucibus,
+                            vitae tincidunt metus auctor. Class aptent taciti sociosqu ad litora torquent per conubia
+                            nostra, per inceptos himenaeos. Curabitur ex risus, eleifend vel scelerisque et, cursus
+                            vitae risus. Vivamus nec sapien ultrices, euismod purus in, elementum purus. Nam vehicula
+                            euismod nunc, vitae pellentesque orci bibendum eget. Class aptent taciti sociosqu ad litora
+                            torquent per conubia nostra, per inceptos himenaeos. Donec ligula sapien, eleifend et mi ut,
+                            tempor feugiat nibh. Vestibulum dui erat, maximus sed ante nec, efficitur tristique lorem.
+                            Duis efficitur urna lorem, vel euismod tortor bibendum nec.
+
+                            Etiam commodo sem vitae lorem viverra pulvinar. Curabitur accumsan commodo interdum. Aenean
+                            lobortis sapien bibendum, tincidunt neque eget, lacinia est. Donec non enim blandit,
+                            consequat felis vel, hendrerit mauris. Cras tincidunt lacus laoreet diam varius, vel congue
+                            nulla laoreet. Sed in porttitor ante. In urna massa, viverra vitae erat quis, lacinia
+                            imperdiet velit. Proin pharetra, purus sit amet viverra dignissim, lectus nisl placerat
+                            turpis, sed scelerisque mauris enim a lacus. Morbi vulputate est non velit fringilla, a
+                            pulvinar dolor vestibulum. Ut tempus lobortis lectus vitae viverra. Ut dui neque, finibus
+                            tempus vehicula sed, efficitur a ligula. Aliquam dapibus felis non enim consequat, sed
+                            luctus magna consectetur. Aenean dictum velit lectus, nec maximus augue porta auctor. Nunc
+                            blandit dapibus sem, vitae scelerisque urna.
+
+                            Sed nec blandit nunc, vitae consequat massa. Nullam fermentum risus justo, consectetur
+                            vulputate nisl suscipit sed. Cras ligula erat, tristique eu enim sed, imperdiet efficitur
+                            neque. Nullam id ipsum nisi. Nulla ullamcorper scelerisque vehicula. Pellentesque pharetra
+                            ut nibh id mollis. Praesent congue dolor porta facilisis congue. Nulla facilisi. Duis eget
+                            mauris blandit arcu pretium cursus. Donec arcu nisl, bibendum et feugiat id, tempus vel
+                            diam.
+
+                            Sed facilisis libero et orci faucibus porttitor. Pellentesque sollicitudin lorem ac tellus
+                            sodales fringilla. Nullam sit amet elit vulputate, ultrices nulla at, suscipit elit.
+                            Praesent facilisis lorem in nulla porta consectetur. Maecenas tincidunt, lacus vitae gravida
+                            eleifend, orci lectus pharetra quam, vel accumsan nibh ante ut nulla. Vivamus placerat
+                            lectus vitae metus placerat, vel venenatis est commodo. Duis tristique, dolor nec
+                            pellentesque euismod, erat dolor venenatis velit, in cursus lorem magna nec lacus. Phasellus
+                            vulputate consectetur lorem vel porta. Fusce id orci ipsum.
+
+                            Pellentesque sodales lacus in lacus placerat venenatis. In leo metus, facilisis et viverra
+                            a, posuere non ligula. Integer at mollis sem. Nunc euismod mi id dapibus suscipit. Sed quis
+                            diam molestie, rutrum elit quis, luctus urna. Cras ac lorem nec eros accumsan commodo. Ut
+                            velit dolor, gravida vitae ornare at, ultricies at quam. Etiam a lectus justo. Nullam
+                            lacinia euismod erat nec tempor. Vivamus suscipit mi id ultricies ornare. Duis augue nunc,
+                            lobortis a lacus nec, blandit iaculis neque. Donec mollis nibh eget orci consectetur
+                            maximus. Ut imperdiet diam sed ligula accumsan, ac ultricies sapien fermentum. Ut sagittis
+                            mattis cursus. Proin vitae sollicitudin elit, vitae euismod erat. Phasellus porta risus
+                            auctor elementum finibus.</p>
                     </div>
                 </div>
             </div>
--- winfrontend/src/components/JavaScriptPage.js
+++ winfrontend/src/components/JavaScriptPage.js
@@ -9,38 +9,75 @@ class JavaScriptPage extends React.Component{
 
     render(){
         return(
-            <div id='javaScriptPage'>
+            <div id='javaPage'>
                 <Navigation/>
-        <div class="row justify-content-md-center">
-        {/* <div class="col-sm-2"></div>
-        <ul class="nav flex-column">
-  <li class="nav-item">
-    <Link class="nav-link active" to={{pathname:'/javascript/react'}}>React</Link>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Other</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Other</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#">Other</a>
-  </li>
-</ul> */}
+                <div className="row">
+                    {/*<div class="col-sm-2" id="sideBar">*/}
+                    <div className="col-sm-1">
+                        <ul className="nav flex-column">
+                            <li className="nav-item">
+                                <Link class="nav-link active" to={{pathname: '/javascript/react'}}>React</Link>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Other</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Other</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Other</a>
+                            </li>
+                        </ul>
+                    </div>
+                    {/*</div>*/}
+                    <div className="col-sm-9 stuff">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam facilisis tortor eu ex faucibus,
+                            vitae tincidunt metus auctor. Class aptent taciti sociosqu ad litora torquent per conubia
+                            nostra, per inceptos himenaeos. Curabitur ex risus, eleifend vel scelerisque et, cursus
+                            vitae risus. Vivamus nec sapien ultrices, euismod purus in, elementum purus. Nam vehicula
+                            euismod nunc, vitae pellentesque orci bibendum eget. Class aptent taciti sociosqu ad litora
+                            torquent per conubia nostra, per inceptos himenaeos. Donec ligula sapien, eleifend et mi ut,
+                            tempor feugiat nibh. Vestibulum dui erat, maximus sed ante nec, efficitur tristique lorem.
+                            Duis efficitur urna lorem, vel euismod tortor bibendum nec.
 
-            <div class="col-sm-8 justify-content-md-center">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempor, risus quis accumsan viverra, purus ante fermentum neque, eu pharetra diam nunc ut arcu. Etiam non mollis nunc. Integer tellus lectus, fermentum sed hendrerit non, pulvinar at ante. Phasellus quis congue libero. Nullam congue velit nec fermentum interdum. Ut scelerisque feugiat odio vitae tincidunt. Proin commodo dolor at euismod aliquam. Nullam blandit molestie erat, ut accumsan erat sodales ac. Suspendisse ultricies mattis quam vitae vehicula. Vestibulum efficitur mi libero, in sollicitudin urna ultricies vitae. Aliquam rutrum lobortis tortor, sed tempor nisl tristique et. In auctor posuere mollis. Vestibulum mi tortor, ornare id justo sit amet, gravida suscipit dui.
+                            Etiam commodo sem vitae lorem viverra pulvinar. Curabitur accumsan commodo interdum. Aenean
+                            lobortis sapien bibendum, tincidunt neque eget, lacinia est. Donec non enim blandit,
+                            consequat felis vel, hendrerit mauris. Cras tincidunt lacus laoreet diam varius, vel congue
+                            nulla laoreet. Sed in porttitor ante. In urna massa, viverra vitae erat quis, lacinia
+                            imperdiet velit. Proin pharetra, purus sit amet viverra dignissim, lectus nisl placerat
+                            turpis, sed scelerisque mauris enim a lacus. Morbi vulputate est non velit fringilla, a
+                            pulvinar dolor vestibulum. Ut tempus lobortis lectus vitae viverra. Ut dui neque, finibus
+                            tempus vehicula sed, efficitur a ligula. Aliquam dapibus felis non enim consequat, sed
+                            luctus magna consectetur. Aenean dictum velit lectus, nec maximus augue porta auctor. Nunc
+                            blandit dapibus sem, vitae scelerisque urna.
 
-Aliquam in sapien dui. Aliquam hendrerit at elit sed pharetra. In tincidunt aliquet neque. Maecenas vitae aliquam augue. Sed tempus non tellus ut imperdiet. Aliquam placerat ante a ipsum elementum dapibus. Etiam porttitor nisl et mi feugiat finibus. Cras posuere urna viverra lobortis tincidunt. Integer et mi placerat, elementum dolor ac, vulputate est. Praesent felis velit, placerat sed arcu vel, finibus consequat nisi. Nullam et lacus eget nunc vulputate vehicula. Proin et orci dignissim, rutrum elit id, placerat sem. Etiam ac facilisis quam. Cras mattis turpis quis imperdiet scelerisque. Nam volutpat quam auctor mauris dapibus ullamcorper.
+                            Sed nec blandit nunc, vitae consequat massa. Nullam fermentum risus justo, consectetur
+                            vulputate nisl suscipit sed. Cras ligula erat, tristique eu enim sed, imperdiet efficitur
+                            neque. Nullam id ipsum nisi. Nulla ullamcorper scelerisque vehicula. Pellentesque pharetra
+                            ut nibh id mollis. Praesent congue dolor porta facilisis congue. Nulla facilisi. Duis eget
+                            mauris blandit arcu pretium cursus. Donec arcu nisl, bibendum et feugiat id, tempus vel
+                            diam.
 
-Etiam quis elit rutrum, viverra metus tincidunt, imperdiet quam. Vestibulum sit amet magna laoreet, varius dolor quis, euismod metus. Sed id dolor turpis. Nam non sapien non erat tempus posuere. Curabitur placerat libero odio, sit amet vestibulum massa semper quis. Aenean orci eros, commodo a tincidunt id, vestibulum posuere ipsum. Sed id dignissim nisi. Morbi congue ultrices vulputate. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor sed quam a congue. In elit dui, tincidunt nec venenatis non, tempus quis leo.
+                            Sed facilisis libero et orci faucibus porttitor. Pellentesque sollicitudin lorem ac tellus
+                            sodales fringilla. Nullam sit amet elit vulputate, ultrices nulla at, suscipit elit.
+                            Praesent facilisis lorem in nulla porta consectetur. Maecenas tincidunt, lacus vitae gravida
+                            eleifend, orci lectus pharetra quam, vel accumsan nibh ante ut nulla. Vivamus placerat
+                            lectus vitae metus placerat, vel venenatis est commodo. Duis tristique, dolor nec
+                            pellentesque euismod, erat dolor venenatis velit, in cursus lorem magna nec lacus. Phasellus
+                            vulputate consectetur lorem vel porta. Fusce id orci ipsum.
 
-Fusce posuere velit in risus porta sodales in a lorem. Pellentesque venenatis justo quis mauris pretium, sed eleifend quam feugiat. Aliquam rhoncus at leo sed malesuada. Sed a ipsum nec lectus dictum gravida. Pellentesque vestibulum ante rutrum massa viverra, eu commodo nunc feugiat. Donec suscipit vitae enim a iaculis. Duis vel urna sed est venenatis rhoncus. Vivamus laoreet, nunc sit amet sollicitudin rutrum, risus nulla mollis justo, at convallis nulla erat eget arcu. Mauris eu lorem eget purus mattis accumsan. Ut orci tortor, commodo at quam ac, fringilla scelerisque magna. Suspendisse condimentum viverra nulla, in venenatis lorem eleifend a. Pellentesque ut gravida dui. Etiam ut massa gravida, cursus ex vitae, gravida tortor. Nulla vel finibus ex. Praesent a dui finibus, lobortis massa et, fringilla nulla. Etiam ullamcorper dolor at lacus malesuada efficitur.
-
-Nulla leo risus, venenatis eget consectetur sit amet, dapibus eget libero. Proin congue feugiat congue. Pellentesque tempus vestibulum augue efficitur porta. Morbi porttitor volutpat purus, at molestie orci. Suspendisse dictum neque id imperdiet gravida. Duis varius massa dictum euismod rutrum. Cras in orci vitae est commodo malesuada quis sed mi. Mauris eu tincidunt tellus, et faucibus ex.</p>
+                            Pellentesque sodales lacus in lacus placerat venenatis. In leo metus, facilisis et viverra
+                            a, posuere non ligula. Integer at mollis sem. Nunc euismod mi id dapibus suscipit. Sed quis
+                            diam molestie, rutrum elit quis, luctus urna. Cras ac lorem nec eros accumsan commodo. Ut
+                            velit dolor, gravida vitae ornare at, ultricies at quam. Etiam a lectus justo. Nullam
+                            lacinia euismod erat nec tempor. Vivamus suscipit mi id ultricies ornare. Duis augue nunc,
+                            lobortis a lacus nec, blandit iaculis neque. Donec mollis nibh eget orci consectetur
+                            maximus. Ut imperdiet diam sed ligula accumsan, ac ultricies sapien fermentum. Ut sagittis
+                            mattis cursus. Proin vitae sollicitudin elit, vitae euismod erat. Phasellus porta risus
+                            auctor elementum finibus.</p>
+                    </div>
+                </div>
             </div>
-</div>
-</div>   
         )
     }
 }
